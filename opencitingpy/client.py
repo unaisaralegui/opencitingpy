5a0fc1847fc0f079c6941c2862f76f75bb3ba7ba
Saralegui Vallejo, Unai <unai.saralegui@tecnalia.com>
2021-05-19 16:03:35 +0200

    first working version

--- opencitingpy/client.py
+++ opencitingpy/client.py
@@ -13,20 +13,105 @@ class Client:
     def __init__(self, api_url=None):
         self.api_url = api_url if api_url is not None else API_URL
 
+    def __construct_url(self, operation, document_identifier, **kwargs):
+        uri = f'{self.api_url}/{operation}/{document_identifier}'
+        return uri
+
+    @staticmethod
+    def __make_request(uri):
+        request = requests.get(uri)
+        data = request.json()
+        return data
+
+    def __get_data(self, operation, document_identifier, **kwargs):
+        uri = self.__construct_url(operation, document_identifier, **kwargs)
+        data = self.__make_request(uri)
+        return data
+
     def get_references(self, doi, **kwargs):
-        raise NotImplementedError("Not available")
+        """
+        This operation retrieves the citation data for all the outgoing references to other
+        cited works appearing in the reference list of the bibliographic entity identified by the input DOI.
+
+        :param doi: A valid Digital Object Identifier (DOI) e.g. '10.3390/s19020353'
+        :type doi: str
+        :param kwargs:
+        :return:
+        """
+        operation = 'references'
+        data = self.__get_data(operation, doi)
+        return data
 
     def get_citation(self, oci, **kwargs):
-        raise NotImplementedError("Not available")
+        """
+        This operation retrieves the citation metadata for the citation identified by
+        the input Open Citation Identifier (OCI).
+
+        :param oci:
+        :param kwargs:
+        :return:
+        """
+        operation = 'citation'
+        data = self.__get_data(operation, oci)
+        return data
 
     def get_citations(self, doi, **kwargs):
-        raise NotImplementedError("Not available")
+        """
+        This operation retrieves the citation data for all the references appearing in the reference lists of other
+        citing works to the bibliographic entity identified by the input DOI, that constitute the incoming citations
+        of that identified bibliographic entity.
+
+        :param doi: A valid Digital Object Identifier (DOI) e.g. '10.3390/s19020353'
+        :type doi: str
+        :param kwargs:
+        :return:
+        """
+        operation = 'citations'
+        data = self.__get_data(operation, doi)
+        return data
 
     def get_metadata(self, dois, **kwargs):
-        raise NotImplementedError("Not available")
+        """
+        This operation retrieves the bibliographic metadata for each of the bibliographic
+        entities identified by one or more input DOIs.
+
+        :param dois: A list of Digital Object Identifierers (DOIs) e.g. ['10.3390/s19020353', '10.3390/s19143113']
+        :type dois: list
+        :param kwargs:
+        :return:
+        """
+        operation = 'metadata'
+        # dois should be a list
+        if isinstance(dois, str):
+            dois = [dois]
+        dois = '__'.join(dois)
+        data = self.__get_data(operation, dois)
+        return data
 
     def get_citation_count(self, doi, **kwargs):
-        raise NotImplementedError("Not available")
+        """
+        This operation retrieves the number of incoming citations to the
+        bibliographic entity identified by the input DOI.
+
+        :param doi: A valid Digital Object Identifier (DOI) e.g. '10.3390/s19020353'
+        :type doi: str
+        :param kwargs:
+        :return:
+        """
+        operation = 'citation-count'
+        data = self.__get_data(operation, doi)
+        return data
 
     def get_reference_count(self, doi, **kwargs):
-        raise NotImplementedError("Not available")
+        """
+        This operation retrieves the number of outgoing citations from the bibliographic
+        entity identified by the input DOI.
+
+        :param doi: A valid Digital Object Identifier (DOI) e.g. '10.3390/s19020353'
+        :type doi: str
+        :param kwargs:
+        :return:
+        """
+        operation = 'reference-count'
+        data = self.__get_data(operation, doi)
+        return data
--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
     install_requires=[
         'requests',
     ],
+    python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
